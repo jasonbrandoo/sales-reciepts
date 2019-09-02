fe7187ac23138a137ce5551eeadf458ea5a56b8f
Jason Brando <jasonbrandoo@gmail.com>
2019-09-02 19:22:03 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Main from './components/Main';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <Main>
-      <h1 className="text-4xl text-center font-bold bg-blue-100 flex-1">
-        TAILWIND ROCK
-      </h1>
-      <h1 className="text-4xl text-center font-bold bg-blue-900 flex-1">
-        TAILWIND ROCK
-      </h1>
-    </Main>
+    <>
+      <Navbar />
+      <Main>
+        <h1 className="text-4xl text-center font-bold bg-blue-100 flex-1">
+          TAILWIND ROCK
+        </h1>
+        <h1 className="text-4xl text-center font-bold bg-blue-900 flex-1">
+          TAILWIND ROCK
+        </h1>
+      </Main>
+    </>
   );
 };
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => {
+    setOpen(state => !state);
+  };
+
+  return (
+    <nav className="flex items-center justify-between flex-wrap bg-blue-500 p-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <span className="font-semibold text-xl tracking-tight">Cashier</span>
+      </div>
+      <div className="block lg:hidden">
+        <button
+          className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+          type="button"
+          onClick={toggle}
+        >
+          <svg
+            className="fill-current h-3 w-3"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+      <div
+        className={
+          open
+            ? 'w-full flex-grow lg:flex lg:items-center lg:w-auto hidden'
+            : 'w-full flex-grow lg:flex lg:items-center lg:w-auto block'
+        }
+      >
+        <div className="text-sm lg:ml-auto">
+          <a
+            href="#logout"
+            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+          >
+            Logout
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
